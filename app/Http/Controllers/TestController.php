a32b7a14e6d3634afdb450897cd8933b82d045d7
汪军 <wangjun@pancool.cn>
2019-02-25 10:10:16 +0800

    jowan

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,28 +7,40 @@ use Illuminate\Http\Request;
 class TestController
 {
   public function jumpTo(Request $request){
-      $i = mt_rand(1,7);
+      $i = 10;//mt_rand(1,10);
       switch ($i){
-          case 1;
-              $title = '随鼠标生成多彩粒子,超好看';
+          case 1:
+              $title = '随鼠标生成多彩粒子,超好看-精美H5动效';
               break;
-          case 2;
-              $title = '3D立体平面图,超炫酷';
+          case 2:
+              $title = '3D立体平面图,超炫酷-精美H5动效';
               break;
-          case 3;
-              $title = '随鼠快速转动的多彩粒子,超酷';
+          case 3:
+              $title = '随鼠快速转动的多彩粒子,超酷-精美H5动效';
               break;
-          case 4;
-              $title = '随鼠生成的炫酷彩带,超动感';
+          case 4:
+              $title = '随鼠生成的炫酷彩带,超动感-精美H5动效';
               break;
-          case 5;
-              $title = '鼠标移动产生的灯光效果,朦胧美';
+          case 5:
+              $title = '鼠标移动产生的灯光效果,朦胧美-精美H5动效';
               break;
-          case 6;
-              $title = '自动生成多彩水泡,非常漂亮';
+          case 6:
+              $title = '自动生成多彩水泡,非常漂亮-精美H5动效';
               break;
-          case 7;
-              $title = '动态雪花效果,超美';
+          case 7:
+              $title = '动态雪花效果,超美-精美H5动效';
+              break;
+          case 8:
+              $title = '炫酷的喷泉灯光效果-精美H5动效';
+              break;
+          case 9:
+              $title = '动态雨滴效果-精美H5动效';
+              break;
+          case 10:
+              $title = '超级好看的烟花效果-精美H5动效';
+              break;
+          default:
+              $title = '啦啦啦';
               break;
       }
 
