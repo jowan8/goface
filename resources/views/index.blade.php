0d64413f1de4703de766fe58f2aa9a9988dea35f
jowan <jowan8@163.com>
2020-05-19 10:44:55 +0800

    '首页添加mate标签'

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,25 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no" /><!--不识别手机号-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /><!--屏幕宽高限定 -->
+    <meta name="apple-mobile-web-app-capable" content="yes" /><!--开启web app的支持-->
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" /><!--顶部栏颜色  default（白色）black（黑色）black-translucent（灰色半透明）-->
+    <link rel="apple-touch-icon-precomposed" href="{{asset('images/ico.ico')}}" /><!--苹果 ico配置-->
+    <link rel="apple-touch-startup-image" href="{{asset('images/ico.ico')}}" /><!--苹果 启动时的照片-->
     <link rel="icon" href={{asset('images/ico.ico')}} type="images/x-ico" />
+
     <title>{{$title}}</title>
 
     <style>
-        body{
-            margin: 0;
+        body {
+            min-width: 320px;
         }
     </style>
 
 </head>
 <body>
+
     学习任务:
     <table>
         <thead>
