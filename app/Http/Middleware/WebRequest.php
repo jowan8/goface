48119e46dcacef0ca13d9f83eccdf5532e26b135
jowan <jowan8@163.com>
2020-06-04 11:53:45 +0800

    忽略本地访问的记录

--- app/Http/Middleware/WebRequest.php
+++ app/Http/Middleware/WebRequest.php
@@ -16,20 +16,21 @@ class WebRequest
      */
     public function handle($request, Closure $next)
     {
-        $data['request_url'] = $request->path();
         $data['client_ip'] = $request->ip();
-        $data['request_data'] = json_encode($request->all());
-
-        $url = 'http://ip-api.com/json/'.$data['client_ip'].'?lang=zh-CN';
-        $return_json = curl_request($url,'GET');
-        $return = json_decode($return_json,true);
-        $data['client_info'] = $return_json;
-        if($return&&$return['status']=='success'){
-            $data['clitent_address'] =  $return['country'].' '.$return['regionName'];
-        }else{
-            $data['clitent_address'] =  '';
+        if($data['client_ip']!='127.0.0.1') {
+            $data['request_data'] = json_encode($request->all());
+            $data['request_url'] = $request->path();
+            $url = 'http://ip-api.com/json/' . $data['client_ip'] . '?lang=zh-CN';
+            $return_json = curl_request($url, 'GET');
+            $return = json_decode($return_json, true);
+            $data['client_info'] = $return_json;
+            if ($return && $return['status'] == 'success') {
+                $data['clitent_address'] = $return['country'] . ' ' . $return['regionName'];
+            } else {
+                $data['clitent_address'] = '';
+            }
+            DB::table('request')->insert($data);
         }
-        DB::table('request')->insert($data);
         return $next($request);
     }
 }
